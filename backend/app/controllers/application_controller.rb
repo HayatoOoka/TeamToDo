257d5c714e2533dc7c12df57afee8b5df46ba7fa
HayatoOoka <140486277+HayatoOoka@users.noreply.github.com>
2025-05-22 15:26:16 +0900

    Delete backend/app/controllers/application_controller.rb

--- backend/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-  def test
-    render json: { message: 'This is a test message from Rails' }
-  end
-  protect_from_forgery with: :null_session
-end
